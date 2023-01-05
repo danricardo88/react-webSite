73094fa5dffbf5161396bb9647ff0157ba095b4e
danricardo88 <daniel.ricardo88@outlook.com>
2023-01-05 03:23:48 -0300

    85%

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,9 +1,39 @@
+/* eslint-disable import/no-dynamic-require */
 import React from 'react';
 import './gallery.css';
+import Header from '../../components/Header';
+import HeaderImage from '../../images/header_bg_3.jpg';
 
 const Gallery = () => {
+  const galleryLength = 15;
+  const images = [];
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`));
+  }
+  // console.log(images);
+
   return (
-    <div>Gallery</div>
+    <>
+      <Header title="Our Gallery" image={HeaderImage}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Nihil repudiandae natus vero rem quo voluptas iste tempora quasi?
+      </Header>
+      <section className="gallery">
+        <div className="container gallery__container">
+          {
+            images.map((image, index) => {
+              return (
+                <article key={index}>
+                  <img src={image} alt={`Gallery Image ${index + 1}`} />
+                </article>
+              );
+            })
+          }
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './notFound.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <section>
+      <div className="container notfound__container">
+        <h2>Page Not Found</h2>
+        <Link to="/" className="btn">Go Back Home</Link>
+      </div>
+    </section>
   );
 };
 
